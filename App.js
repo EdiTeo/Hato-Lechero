f324996dff5b920042e7d25cf36714cb925851e6
edith.teodocia@gmail.com <edith.teodocia@gmail.com>
2024-11-02 15:58:10 -0400

    #03::Tab Seccion Animales

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Inicio from './screens/Inicio';
 
+
 const App = () =>{
   return <Inicio/>;
 };
--- screens/Inicio.js
+++ screens/Inicio.js
@@ -6,8 +6,12 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import Control from './tabnavigator/Control';
 import Barra from './tabnavigator/Barra';
-
-
+import FormularioAddVaca from './tabnavigator/Animales/FormularioAddVaca'; 
+import Vacas from './tabnavigator/Animales/Vacas'; 
+ 
+//=====================================================
+//AÑADIR A ESTA SECCION LAS VENTANAS O REDIRECCIONAMIENTOS QUE SE QUIERE MOSTRAR AL HACER CLIC 
+//=====================================================
 const Stack = createStackNavigator();
 
 const Inicio = () => {
@@ -22,7 +26,20 @@ const Inicio = () => {
         name="Barra" 
         component={Barra} 
         options={{ headerShown: false }} />
+
+        <Stack.Screen 
+        name="Vacas" 
+        component={Vacas} 
+        options={{ headerShown: false }} />
+       
+
+        <Stack.Screen 
+          name="FormularioAddVaca" 
+          component={FormularioAddVaca}
+          options={{ headerShown: false }} 
+        />
       </Stack.Navigator>
+      
     </NavigationContainer>
   );
 };
